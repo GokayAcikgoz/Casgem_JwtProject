21f9dade048b5607b86c522344824dbc97450ef2
gokay <gokayacikgoz1@gmail.com>
2023-08-01 14:41:58 +0300

    Add project files.

---
+++ Casgem_JwtProject/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Casgem_JwtProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casgem_JwtProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("Action")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet("getall")]
+        public IActionResult CustomerList()
+        {
+            return Ok("Müşteri listesine eriştiniz");
+        }
+    }
+}
